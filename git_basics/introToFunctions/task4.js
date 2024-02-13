9958e367e002fd9c6f4250f96fbc7dcd81f88833
LenaTester <lena.tester2021@gmail.com>
2024-02-13 08:55:43 +0100

    added_tasks_for_functions

---
+++ git_basics/introToFunctions/task4.js
@@ -0,0 +1,12 @@
+// checking, if number is even or odd
+
+function isEven(number) {
+    if (number % 2 == 0) {
+        console.log(true)
+    } else {
+        console.log(false)
+    }
+ }
+
+ isEven(10)
+ isEven(11)
