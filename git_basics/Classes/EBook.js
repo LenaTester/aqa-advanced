9e6c3ae44a71a0044fe8d1a28da4c07abefe5d86
LenaTester <lena.tester2021@gmail.com>
2024-02-27 10:24:26 +0100

    added_tasks_for_classes

---
+++ git_basics/Classes/EBook.js
@@ -0,0 +1,36 @@
+import { Book } from "./Book.js"
+
+// creation of electronic book class
+export class Ebook extends Book {
+
+    constructor (title, author, year, fileFormat) {
+        super(title, author, year)
+        this._fileFormat = fileFormat
+    }
+
+    // creation of getter and setter for eletronic book specific values
+    get showFileFormat() {
+        return this._fileFormat
+    }
+
+    set showFileFormat(value) {
+        if (typeof value !== 'string') {
+            console.log('File format should be a string')
+        }
+        return this._fileFormat = value
+    }
+
+    // printing info about electonic book
+    printInfo() {
+        super.printInfo() + console.log(`File format: ${this._fileFormat}`)
+      }
+
+      // transforming book into electronic book
+      static transformBook(book, fileFormat) {
+        console.log(new Ebook(book._title, book._author, book._year, fileFormat))
+      }
+    }
+
+
+
+
